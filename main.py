1e1fd5adf2d67eaae7965f15868a65a1c672514c
Vishesh Didwaniya <vishesh.2409@gmail.com>
2021-07-22 04:48:15 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,145 @@
+import string
+import pandas as pd
+from flask import Flask, render_template, request
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import bs4 as bs
+import urllib.request
+
+def create_similarity():
+    data = pd.read_csv('main_data.csv')
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(data['comb'])
+    similarity = cosine_similarity(count_matrix)
+    return data, similarity
+
+
+def rcmd(m):
+    m = m.lower()
+    try:
+        data.head()
+        similarity.shape
+    except:
+        data, similarity = create_similarity()
+    if m not in data['movie_title'].unique():
+        return (
+            'Sorry! The movie you requested is not in our database. Please check the spelling or try with some other movies')
+    else:
+        i = data.loc[data['movie_title'] == m].index[0]
+        lst = list(enumerate(similarity[i]))
+        lst = sorted(lst, key=lambda x: x[1], reverse=True)
+        lst = lst[1:11]
+        l = []
+        for i in range(len(lst)):
+            a = lst[i][0]
+            l.append(data['movie_title'][a])
+        return l
+
+
+def convert_to_list(my_list):
+    my_list = my_list.split('","')
+    my_list[0] = my_list[0].replace('["', '')
+    my_list[-1] = my_list[-1].replace('"]', '')
+    return my_list
+
+
+def get_suggestions():
+    data = pd.read_csv('main_data.csv')
+    return list(data['movie_title'].str.capitalize())
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    suggestions = get_suggestions()
+    return render_template('home.html', suggestions=suggestions)
+
+
+@app.route("/similarity", methods=["POST"])
+def similarity():
+    movie = request.form['name']
+    rc = rcmd(movie)
+    if type(rc) == type('string'):
+        return rc
+    else:
+        m_str = "---".join(rc)
+        return m_str
+
+
+@app.route("/recommend", methods=["POST"])
+def recommend():
+    title = request.form['title']
+    cast_ids = request.form['cast_ids']
+    cast_names = request.form['cast_names']
+    cast_chars = request.form['cast_chars']
+    cast_bdays = request.form['cast_bdays']
+    cast_bios = request.form['cast_bios']
+    cast_places = request.form['cast_places']
+    cast_profiles = request.form['cast_profiles']
+    imdb_id = request.form['imdb_id']
+    poster = request.form['poster']
+    genres = request.form['genres']
+    overview = request.form['overview']
+    vote_average = request.form['rating']
+    vote_count = request.form['vote_count']
+    release_date = request.form['release_date']
+    runtime = request.form['runtime']
+    status = request.form['status']
+    rec_movies = request.form['rec_movies']
+    rec_posters = request.form['rec_posters']
+
+    suggestions = get_suggestions()
+
+    rec_movies = convert_to_list(rec_movies)
+    rec_posters = convert_to_list(rec_posters)
+    cast_names = convert_to_list(cast_names)
+    cast_chars = convert_to_list(cast_chars)
+    cast_profiles = convert_to_list(cast_profiles)
+    cast_bdays = convert_to_list(cast_bdays)
+    cast_bios = convert_to_list(cast_bios)
+    cast_places = convert_to_list(cast_places)
+
+    cast_ids = cast_ids.split(',')
+    cast_ids[0] = cast_ids[0].replace("[", "")
+    cast_ids[-1] = cast_ids[-1].replace("]", "")
+
+    for i in range(len(cast_bios)):
+        cast_bios[i] = cast_bios[i].replace(r'\n', '\n').replace(r'\"', '\"')
+
+    for i in range(len(rec_posters)):
+        rec_movies[i] = string.capwords(rec_movies[i])
+
+    movie_cards = {rec_posters[i]: rec_movies[i] for i in range(len(rec_posters))}
+
+    casts = {cast_names[i]: [cast_ids[i], cast_chars[i], cast_profiles[i]] for i in range(len(cast_profiles))}
+
+    cast_details = {cast_names[i]: [cast_ids[i], cast_profiles[i], cast_bdays[i], cast_places[i], cast_bios[i]] for i in
+                    range(len(cast_places))}
+
+    sauce = urllib.request.urlopen('https://www.imdb.com/title/{}/reviews?ref_=tt_ov_rt'.format(imdb_id)).read()
+    soup = bs.BeautifulSoup(sauce, 'lxml')
+    soup_result = soup.find_all("div", {"class": "text show-more__control"})
+    soup_result2 = soup.find_all("div", {"class": "ipl-ratings-bar"})
+
+    reviews_list = []
+    reviews_rating = []
+    for reviews in soup_result:
+        if reviews.string:
+            reviews_list.append(reviews.string)
+
+    for review in soup_result2:
+        reviews_rating.append(review.span.span.string)
+
+    movie_reviews = {reviews_list[i]: reviews_rating[i] for i in range(len(reviews_list))}
+
+    return render_template('recommend.html', title=title, poster=poster, overview=overview, vote_average=vote_average,
+                           vote_count=vote_count, release_date=release_date, runtime=runtime, status=status,
+                           genres=genres,
+                           movie_cards=movie_cards, reviews=movie_reviews, casts=casts, cast_details=cast_details)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
